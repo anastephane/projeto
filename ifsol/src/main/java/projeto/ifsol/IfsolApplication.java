542b59555720492484e051a8e5887be2cb656a59
Clara Damasceno <mariaclarasmatos97@gmail.com>
2019-12-06 09:02:30 -0200

    Atualizações de configurações básicas.

--- ifsol/src/main/java/projeto/ifsol/IfsolApplication.java
+++ ifsol/src/main/java/projeto/ifsol/IfsolApplication.java
@@ -2,12 +2,16 @@ package projeto.ifsol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import projeto.ifsol.model.Servidores;
 
 @SpringBootApplication
 public class IfsolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IfsolApplication.class, args);
+		System.out.print(new BCryptPasswordEncoder().encode("123"));
 	}
 
 }
